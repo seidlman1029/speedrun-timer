b9002772b8441453f360ecb3f0f998e489ad10c9
Chris Seidl <cseidl@coldlight.com>
2019-08-19 21:06:25 -0400

    Mediocre program woo

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace speedrun_timer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                Console.WriteLine("Press any key to start...");
+                Console.ReadKey();
+                Console.WriteLine("\nTiming...");
+
+                stopwatch.Start();
+                Console.ReadKey();
+                stopwatch.Stop();
+                Console.WriteLine($"\nElapsed time: {stopwatch.ElapsedMilliseconds} millis");
+            }
+        }
+    }
+}
